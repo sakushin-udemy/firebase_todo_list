2a21ea2a2daa1897dad2b4810fdfb016cd708562
sakushin <sakushin.udemy@gmail.com>
2023-02-01 10:43:51 +0900

    ユーザ情報入力用のダイアログの外枠だけ作成した

---
+++ lib/login_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginDialog extends StatefulWidget {
+  const LoginDialog({Key? key}) : super(key: key);
+
+  @override
+  State<LoginDialog> createState() => _LoginDialogState();
+}
+
+class _LoginDialogState extends State<LoginDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_todo_list/login_dialog.dart';
 import 'package:firebase_todo_list/repositories/todo_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -188,7 +189,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _onAddTodo,
+        onPressed: //_onAddTodo,
+            () => showDialog(
+                context: context, builder: (context) => LoginDialog()),
         tooltip: 'add todo',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
