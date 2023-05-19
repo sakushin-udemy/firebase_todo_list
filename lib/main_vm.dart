40e78d7cd57b1a75fb48ed81ed711e28b3b13432
sakushin <sakushin.udemy@gmail.com>
2023-05-20 06:39:27 +0900

    riverpod generator用の todoRepositoryを追加

--- lib/main_vm.dart
+++ lib/main_vm.dart
@@ -1,6 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_todo_list/data/main_model.dart';
+import 'package:firebase_todo_list/repositories/todo_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'main_vm.g.dart';
+
+@riverpod
+Stream<QuerySnapshot<Map<String, dynamic>>> todoRepository(
+  ref, {
+  required String userId,
+  required bool visibleDoneItem,
+  required bool descending,
+}) {
+  return TodoRepository(userId)
+      .stream(
+        isDone: visibleDoneItem ? null : false,
+        sortMethod: SortMethod.deadlineTime,
+        descending: descending,
+      )
+      .snapshots();
+}
 
 @riverpod
 class MainVm extends _$MainVm {
